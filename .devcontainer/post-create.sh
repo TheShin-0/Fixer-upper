11e36a8b1d063fed2b5d2b6291021b431c28dd1a
TheShin-0 <158040677+TheShin-0@users.noreply.github.com>
2024-10-15 09:05:36 +0000

    working build

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-sudo apt-get updates
-sudo apt-get install texlive-xetex -y
-mamba env create -f gammapy-1.2-environment.yml
-mamba create --name fermipy -c conda-forge -c fermi python=3.9 "fermitools>=2.2.0" healpy gammapy jupyter
-source activate fermipy
+sudo apt-get update
+sudo apt install texlive-xetex -y
+conda create --name fermipy -c conda-forge -c fermi python=3.9 "fermitools>=2.2.0" healpy gammapy jupyter
+conda activate fermipy
 pip install fermipy==1.3.0
+conda activate base
+conda env create -f gammapy-1.2-environment.yml -y
