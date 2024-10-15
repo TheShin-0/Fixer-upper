fccfb105936c082ce3605a84478db825320c0832
TheShin-0 <158040677+TheShin-0@users.noreply.github.com>
2024-10-15 07:55:44 +0000

    save

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sudo apt-get updates
+sudo apt-get install texlive-xetex -y
 mamba env create -f gammapy-1.2-environment.yml
 mamba create --name fermipy -c conda-forge -c fermi python=3.9 "fermitools>=2.2.0" healpy gammapy jupyter
 source activate fermipy
